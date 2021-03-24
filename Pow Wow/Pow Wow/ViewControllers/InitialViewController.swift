d9ac258c90561a3ec609e194c018900f48d3ef8d
jacksonmccluskey <jacksonmccluskey@protonmail.com>
2021-03-24 18:57:40 -0400

    loginToLogin UI complete

--- Pow Wow/Pow Wow/ViewControllers/InitialViewController.swift
+++ Pow Wow/Pow Wow/ViewControllers/InitialViewController.swift
@@ -11,19 +11,9 @@ class InitialViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func loginButton(_ sender: Any) {
+        performSegue(withIdentifier: "loginToLogin", sender: self)
     }
-    */
-
 }
